4039cd0ba80a5f4d99413ca29af15e2805f938b1
sezin akbulut <sezin.akbulut@gmail.com>
2024-02-27 09:23:13 +0100

    create WeatherApp component

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherApp.css";
+
+const WeatherApp = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData("Gent");
+  }, []);
+
+  const fetchWeatherData = async (cityInput) => {
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=6b0be4b511734f20b8e92852231411&q=${cityInput}&q=07112&days=7`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      alert("City not found, please try again");
+    }
+  };
+
+  return (
+    <div className="weather-app">
+      {/* Rest of your JSX code goes here */}
+      {/* Use weatherData to render the weather information */}
+      {weatherData && <WeatherDetails data={weatherData} />}
+    </div>
+  );
+};
+
+export default WeatherApp;

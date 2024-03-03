a9444ff5778d687643feafdfd1c67c81e3f764e2
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-03 19:52:49 +0100

    update weekly forecast

--- src/WeeklyForecast.jsx
+++ src/WeeklyForecast.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./style/WeeklyForecast.css";
 
 const WeeklyForecast = ({ weeklyForecast }) => {
+  if (!weeklyForecast || !Array.isArray(weeklyForecast)) {
+    // Handle the case where weeklyForecast is undefined or not an array
+    return null;
+  }
+
   const getDayName = (dateString) => {
     const options = { weekday: "long" };
     return new Date(dateString).toLocaleDateString(undefined, options);
@@ -10,7 +15,7 @@ const WeeklyForecast = ({ weeklyForecast }) => {
   return (
     <div className="days-forecast">
       <ul className="weather-cards">
-        {weeklyForecast.slice(0, 3).map((day) => (
+        {weeklyForecast.slice(0, 5).map((day) => (
           <li key={day.date} className="card">
             <div>
               <div className="city-timeweekly">
@@ -19,17 +24,21 @@ const WeeklyForecast = ({ weeklyForecast }) => {
                 </small>
               </div>
               <div className="weatherweekly">
-                <img
-                  className="icondaily"
-                  src={`https:${day.day.condition.icon}`}
-                  alt="icon"
-                  width="50"
-                  height="50"
-                />
-                <h1 className="tempweekly">{day.day.maxtemp_c}&#176;</h1>
-                <span className="conditionweekly">
-                  {day.day.condition.text}
-                </span>
+                {day.day && day.day.condition && (
+                  <>
+                    <img
+                      className="icondaily"
+                      src={`https:${day.day.condition.icon}`}
+                      alt="icon"
+                      width="50"
+                      height="50"
+                    />
+                    <h1 className="tempweekly">{day.day.maxtemp_c}&#176;</h1>
+                    <span className="conditionweekly">
+                      {day.day.condition.text}
+                    </span>
+                  </>
+                )}
               </div>
             </div>
           </li>
